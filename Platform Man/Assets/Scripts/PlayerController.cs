14300357bbf2292fc0440b516e514d906a97ee6b
GBrandt <gbrandt@mail.bradley.edu>
2020-09-21 15:12:41 -0700

    Added some place holder assets and player controller script

---
+++ Platform Man/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿/*
+* (Greg Brandt)
+* (Project 2)
+* Allows the player to respond to input
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+	[SerializeField] float jumpForce = 1;
+	[SerializeField] float fallingVelocityJumpThreshold = 0.2f;
+	public Vector2 velocity;
+
+	//Compensates for the height of the collider 
+	float distToGround;
+
+	Rigidbody2D rigidbody;
+	bool isOnGround = false;
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		distToGround = GetComponent<BoxCollider2D>().bounds.extents.y;
+	}
+
+	private void Update()
+	{
+		
+		//Move Left and Right
+		float horizontalInput = Input.GetAxis("Horizontal");
+		transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+	}
+
+	private void FixedUpdate()
+	{
+		//Jump
+		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+		{
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
+	}
+
+	//Checks if there is a collider under the player
+	bool IsGrounded()
+	{
+		RaycastHit2D hit =  Physics2D.Raycast(transform.position - new Vector3(0, distToGround + 0.1f, 0), -Vector3.up, 0.5f);
+		if (hit)
+		{
+			Debug.Log(hit.collider.tag);
+			if (!hit.collider.CompareTag("Player")) { return hit; }
+			else { return false; }
+		}
+		return false;
+	}
+}
