e6666eafb12cc4ca9e430bcbf798da38325419ed
minichibis <minichibis@gmail.com>
2020-09-25 21:12:51 -0500

    Tile / Player / There's Not Really a Difference
    Tiles now exist for the player to gallavant along. These use unity's tilemap system.
    
    In addition, the Player is made up of tilemaps as well, a functional one and an additional visual one. The player can have fully arbitrary shapes that collide with others in a logical manner.
    
    Transformation is also semi functional. Pressing "Z" transforms one of the players into a block.  This block collides as expected with the player, and even when both active collisions are still sane.
    
    Do keep in mind walls do not work, however.

--- Platform Man/Assets/Scripts/PlayerController.cs
+++ Platform Man/Assets/Scripts/PlayerController.cs
@@ -1,36 +1,44 @@
 ﻿/*
-* (Greg Brandt)
+* (Greg Brandt, Sam Carpenter)
 * (Project 2)
 * Allows the player to respond to input
 */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 1;
 	[SerializeField] float jumpForce = 1;
 	[SerializeField] float fallingVelocityJumpThreshold = 0.2f;
-	[SerializeField] bool isBlock;
+	//[SerializeField] bool isBlock;
 	public Vector2 velocity;
-	public bool isPlayable = false;
+	public bool isPlayable = true;
+	
+	public int coyoteframes = 0;
+	public int coyotethresh = 3;
+	
+	public int gravy = 3;
 
 	private void Start()
 	{
-		if(!isBlock) { isPlayable = true; }
+		//if(!isBlock) { isPlayable = true; }
 	}
 
 
 	//Compensates for the height of the collider 
-	float distToGround;
+	float distToGround = 1.15f;
 
 	Rigidbody2D rigidbody;
+	public Tilemap pinktiles;
+	public Tilemap greytiles;
 
 	private void Awake()
 	{
-		rigidbody = GetComponent<Rigidbody2D>();
-		distToGround = GetComponent<BoxCollider2D>().bounds.extents.y;
+		rigidbody =  GetComponentInChildren<Rigidbody2D>();
+		//distToGround = GetComponent<Collider2D>().bounds.extents.y;
 	}
 
 	private void Update()
@@ -40,27 +48,52 @@ public class PlayerController : MonoBehaviour
 			//Move Left and Right
 			float horizontalInput = Input.GetAxis("Horizontal");
 			transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+			//for use with transforming
+			rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+			rigidbody.gravityScale = gravy;
+			pinktiles.color = Color.white;
+			greytiles.color = Color.clear;
+		} else{
+			//for use with transforming
+			rigidbody.constraints = RigidbodyConstraints2D.FreezePosition | RigidbodyConstraints2D.FreezeRotation;
+			rigidbody.gravityScale = 0;
+			pinktiles.color = Color.clear;
+			greytiles.color = Color.white;
 		}
 	}
+	
+	//variable for testing purposes. expendable
+	public bool canTransform = true;
 
 	private void FixedUpdate()
 	{
+		bool g = IsGrounded();
 		//Jump
-		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() && !isBlock)
+		if (Input.GetKeyDown(KeyCode.Space) && isPlayable && (g || coyoteframes <= coyotethresh))
 		{
-			GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+			rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+			coyoteframes = coyotethresh + 1;
 		}
+		
+		if(!g){
+			coyoteframes++;
+		} else{
+			coyoteframes = 0;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Z) && canTransform) isPlayable = !isPlayable;
 	}
 
 	//Checks if there is a collider under the player
 	bool IsGrounded()
 	{
-		RaycastHit2D hit =  Physics2D.Raycast(transform.position - new Vector3(0, distToGround + 0.1f, 0), -Vector3.up, 0.5f);
-		if (hit)
-		{
-			Debug.Log(hit.collider.tag);
-			if (!hit.collider.CompareTag("Player")) { return hit; }
-			else { return false; }
+		RaycastHit2D[] hitsys = new RaycastHit2D[999];
+		int hitnum = rigidbody.Cast(Vector2.down, hitsys, distToGround);
+		if (hitnum > 0){
+			//Debug.Log(hit.collider.tag);
+			//if (!hit.collider.CompareTag("Player")) { return hit; }
+			//else { return false; }
+			return true;
 		}
 		return false;
 	}
