71f065625a8fb2e1088af158c84815d06edef8cc
Gregory Blevins <gblevins@mail.bradley.edu>
2020-10-04 17:40:46 -0500

    PauseMenu update
    The ability to pause is added

--- Platform Man/Assets/Scripts/LevelController.cs
+++ Platform Man/Assets/Scripts/LevelController.cs
@@ -13,12 +13,29 @@ public class LevelController : MonoBehaviour
     //This array of scenes of all the levels
     public string[] levels = {"MainMenu", "SampleScene", "Tutorial1", "Tutorial2" , "Tutorial3" , "Tutorial4"};
 
+    public Canvas pauseMenu;
+
     int failureCount = 0;
 
-    // Start is called before the first frame update
-    void Start()
+    private void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pauseMenu.gameObject.activeInHierarchy)
+            {
+                pauseMenu.gameObject.SetActive(true);
+                
+            }
+            else if (pauseMenu.gameObject.activeInHierarchy)
+            {
+                ResumeLevel();
+            }
+        }
+    }
+
+    public void ResumeLevel()
+    {
+        pauseMenu.gameObject.SetActive(false);
     }
 
     //Restarts the current level
