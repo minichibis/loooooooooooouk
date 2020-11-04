7f411975db51f64da730083a4f371903558d4141
Gregory Blevins <gblevins@mail.bradley.edu>
2020-11-04 16:14:31 -0600

    Fixed Rotation
    Fixed monster rotation, but introduced a new movement bug.

--- Greater Good/Assets/Scripts/FollowPlayerMainMonster.cs
+++ Greater Good/Assets/Scripts/FollowPlayerMainMonster.cs
@@ -7,18 +7,14 @@ public class FollowPlayerMainMonster: MonoBehaviour
     [SerializeField] float moveSpeed;
 
     Vector3 targetPosition;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate((targetPosition - transform.position).normalized * moveSpeed * Time.deltaTime);
-        
-        //transform.LookAt(targetPosition);
+
+        transform.LookAt(targetPosition);
     }
 
     public void UpdateTarget(Vector3 updatedPosition)
