d7682a727916638306951e1ba4733e851355119e
Gregory Blevins <gblevins@mail.bradley.edu>
2020-11-30 16:17:21 -0600

    Chaser Update

---
+++ Greater Good/Assets/Scripts/ChaserBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserBehaviour : MonoBehaviour
+{
+    List<MonsterPathing> previousNodes;
+
+    private int maxPossibleNodes = 12;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public List<Vector3> PathToTravel()
+    {
+        List<Vector3> navigationalNodes = new List<Vector3>();
+
+        for (int i = 0; i <= previousNodes.Count; i++)
+        {
+            navigationalNodes.Add(previousNodes[i].transform.position);
+        }
+
+        return navigationalNodes;
+    }
+}
